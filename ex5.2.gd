a750fc2431b692f291ac5fb5aa8a02036ea0be2b
ido <games@ido.wtf>
2022-05-24 14:46:04 +0300

    Godot implementation for EX 5.2 of https://natureofcode.com/book/chapter-5-physics-libraries/

---
+++ ex5.2.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	if(Input.is_action_just_pressed("click")): 
+		var m_pos = get_viewport().get_mouse_position()
+		var unit = load("res://block.tscn").instance()
+		add_child(unit)
+		unit.position = m_pos 
